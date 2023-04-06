59d2c828f3cd351ad9e2d92a079a28795b7e29a7
NQuiros12 <nicolasquiros12@gmail.com>
2023-04-05 21:03:58 -0300

    Commit inicial para la creacion del back

---
+++ VRS/src/test/java/vrs/backend/demo/VrsApplicationTests.java
@@ -0,0 +1,13 @@
+package vrs.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
