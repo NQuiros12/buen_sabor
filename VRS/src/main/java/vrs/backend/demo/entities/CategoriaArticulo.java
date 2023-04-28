797409c7dd9d077c900a1ca1014e9287cbf617e5
NQuiros12 <nicolasquiros12@gmail.com>
2023-04-28 18:16:07 -0300

    Add the recursive class, I just commited to test it in my Unix env

---
+++ VRS/src/main/java/vrs/backend/demo/entities/CategoriaArticulo.java
@@ -0,0 +1,38 @@
+package vrs.backend.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import vrs.backend.demo.generics.entities.Base;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name="CategoriaArticulo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoriaArticulo extends Base {
+    @Column(name="denominacion")
+    private String denominacion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private CategoriaArticulo parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<CategoriaArticulo> children = new ArrayList<>();
+
+    public void addChild(CategoriaArticulo child) {
+        children.add(child);
+        child.setParent(this);
+    }
+
+    public void removeChild(CategoriaArticulo child) {
+        children.remove(child);
+        child.setParent(null);
+    }
+}
