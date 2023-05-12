7cc863de0faec5e279d27d098404eccbf4a787e7
FedericoPitton <fedejpitton@gmail.com>
2023-05-12 10:24:41 -0300

    Borro codigo comentado

--- VRS/src/main/java/vrs/backend/demo/entities/Producto.java
+++ VRS/src/main/java/vrs/backend/demo/entities/Producto.java
@@ -1,14 +1,9 @@
 package vrs.backend.demo.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import vrs.backend.demo.generics.entities.Base;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "Producto")
@@ -24,17 +19,5 @@ public class Producto extends Base {
 
     @Column(name = "precioVenta")
     private double precio_venta;
-    /*
-    @OneToMany(mappedBy = "producto")
-    private List<DetalleFactura> detalleFacturas = new ArrayList<DetalleFactura>();
-    @OneToMany(mappedBy = "producto")
-    private List<DetallePedido> detallePedidos = new ArrayList<DetallePedido>();
 
-    @OneToOne(mappedBy = "producto")
-    private ArticuloInsumo articuloInsumo;
-
-    @OneToOne(mappedBy = "producto")
-    private ArticuloManufacturado articuloManufacturado;
-
-     */
 }
