2d9247a9898e9d8baed4752d7598592679c2bd24
NQuiros12 <nicolasquiros12@gmail.com>
2023-05-23 08:00:43 -0300

    agrego busqueda por nombre(denominacion) para articuloManufacturado

--- VRS/src/main/java/vrs/backend/demo/repositories/ArticuloManufacturadoRepository.java
+++ VRS/src/main/java/vrs/backend/demo/repositories/ArticuloManufacturadoRepository.java
@@ -1,11 +1,16 @@
 package vrs.backend.demo.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vrs.backend.demo.entities.ArticuloManufacturado;
 import vrs.backend.demo.generics.repositories.BaseRepository;
 
+import java.util.List;
+
 @Repository
 public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado,Long> {
 
-
+    @Query("SELECT artMan FROM ArticuloManufacturado artMan WHERE artMan.denominacion like concat('%',:nombreArtMan,'%') order by artMan.precioVenta")
+    List<ArticuloManufacturado> findByName(@Param("nombreArtMan") String nombreArtMan);
 }
