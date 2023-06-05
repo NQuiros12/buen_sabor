af650de0d1571694ce6e22236be78dd5445ce887
NQuiros12 <nicolasquiros12@gmail.com>
2023-06-05 18:31:34 -0300

    Search categoria por denominacion

--- VRS/src/main/java/vrs/backend/demo/controllers/CategoriaArticuloController.java
+++ VRS/src/main/java/vrs/backend/demo/controllers/CategoriaArticuloController.java
@@ -1,17 +1,27 @@
 package vrs.backend.demo.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import vrs.backend.demo.entities.ArticuloInsumo;
+import vrs.backend.demo.entities.ArticuloManufacturado;
 import vrs.backend.demo.entities.CategoriaArticulo;
 import vrs.backend.demo.generics.controllers.implementation.BaseControllerImpl;
+import vrs.backend.demo.repositories.ArticuloManufacturadoRepository;
+import vrs.backend.demo.repositories.CategoriaArticuloRepository;
 import vrs.backend.demo.services.CategoriaArticuloService;
 import vrs.backend.demo.services.implementation.ArticuloInsumoServiceImpl;
 import vrs.backend.demo.services.implementation.CategoriaArticuloServiceImpl;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/categorias")
 public class CategoriaArticuloController extends BaseControllerImpl<CategoriaArticulo, CategoriaArticuloServiceImpl> {
+    @Autowired
+    private CategoriaArticuloRepository categoriaArticuloRepository;
+    @GetMapping("/buscar_nombre/{nombreArtMan}")
+    public List<CategoriaArticulo> searchByName(@PathVariable String nombreCategoria) {
+        return categoriaArticuloRepository.findByName(nombreCategoria);
+    }
 }
--- VRS/src/main/java/vrs/backend/demo/repositories/CategoriaArticuloRepository.java
+++ VRS/src/main/java/vrs/backend/demo/repositories/CategoriaArticuloRepository.java
@@ -1,9 +1,16 @@
 package vrs.backend.demo.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import vrs.backend.demo.entities.ArticuloManufacturado;
 import vrs.backend.demo.entities.CategoriaArticulo;
 import vrs.backend.demo.generics.repositories.BaseRepository;
 
+import java.util.List;
+
 @Repository
 public interface CategoriaArticuloRepository extends BaseRepository<CategoriaArticulo,Long> {
+    @Query("SELECT nombreCategoria FROM CategoriaArticulo nombreCategoria WHERE nombreCategoria.denominacion like concat('%',:nombreCategoria,'%') ")
+    List<CategoriaArticulo> findByName(@Param("nombreCategoria") String nombreCategoria);
 }
