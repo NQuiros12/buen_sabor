eac7fb0acfaa27e05fe33485b348036c5c80e459
FedericoPitton <fedejpitton@gmail.com>
2023-06-14 18:21:50 -0300

    Agrego metodo para crear Pedido y modificarlo, tambien actualiza el stock de los ingredientes si el estado del pedido pasa a ser 'ACEPTADO'(4)

--- VRS/src/main/java/vrs/backend/demo/controllers/FacturaController.java
+++ VRS/src/main/java/vrs/backend/demo/controllers/FacturaController.java
@@ -1,14 +1,27 @@
 package vrs.backend.demo.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vrs.backend.demo.entities.Factura;
 import vrs.backend.demo.generics.controllers.implementation.BaseControllerImpl;
+import vrs.backend.demo.services.implementation.DetalleFacturaServiceImpl;
+import vrs.backend.demo.services.implementation.DetallePedidoServiceImpl;
 import vrs.backend.demo.services.implementation.FacturaServiceImpl;
+import vrs.backend.demo.services.implementation.PedidoServiceImpl;
 
 @RestController
 @CrossOrigin(origins = "*")
+@AllArgsConstructor
 @RequestMapping(path = "/factura")
 public class FacturaController extends BaseControllerImpl<Factura, FacturaServiceImpl> {
+
+
+    private final DetalleFacturaServiceImpl detalleFacturaServiceImpl;
+    private final FacturaServiceImpl facturaServiceImpl;
+
+
+
 }
