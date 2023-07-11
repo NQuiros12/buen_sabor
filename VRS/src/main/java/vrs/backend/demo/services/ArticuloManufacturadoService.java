b7472bda77683e0440f2988face3d6f0330afce5
FedericoPitton <fedejpitton@gmail.com>
2023-07-10 21:25:54 -0300

    Agrego metodos al servicio para luego implementarlo, pedido y articuloManufacturado

--- VRS/src/main/java/vrs/backend/demo/services/ArticuloManufacturadoService.java
+++ VRS/src/main/java/vrs/backend/demo/services/ArticuloManufacturadoService.java
@@ -3,6 +3,11 @@ package vrs.backend.demo.services;
 import vrs.backend.demo.entities.ArticuloManufacturado;
 import vrs.backend.demo.generics.services.BaseService;
 
+import java.util.List;
+
 
 public interface ArticuloManufacturadoService extends BaseService<ArticuloManufacturado, Long> {
+    void updateArticuloManufacturado(ArticuloManufacturado articuloRecibido, Long idPrevio) throws Exception;
+    void saveArticuloManufacturado(ArticuloManufacturado articuloManufacturado) throws Exception;
+    List<ArticuloManufacturado> buscarPorNombre(String nombreArtMan);
 }
